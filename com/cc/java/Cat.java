d8bd6645efb4a6b330bbcee5a212a798cd935282
Arian <a.hormozfar@yahoo.de>
2024-07-18 12:07:37 +0200

    Vererbung Anpassung

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,29 +1,13 @@
 package com.cc.java;
 
-public class Cat {
-
-  private String name;
-  private String furColor;
-  private int age; 
+public class Cat extends SuperCat{
 
   private int counter;
   
   public Cat(String name, String furColor, int age) {
-    this.name = name;
-    this.furColor = furColor;
-    this.age = age;
+    super(name, furColor, age);
   }
 
-  public String getStringAttributes(String op) {
-    switch (op) {
-      case "#name":
-        return name;
-      case "#color":
-        return furColor;
-      default:
-        return "ERROR!";
-    }
-  }
 
   public String getAge() {
         return checkCompliance();
